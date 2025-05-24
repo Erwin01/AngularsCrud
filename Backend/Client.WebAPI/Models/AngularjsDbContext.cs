2417e5a0dafd63d5bb739424cd31b22278ae5dc5
Erwin <erwin.paralesda@gmail.com>
2025-05-24 15:41:01 -0500

    Add files via upload

---
+++ Backend/Client.WebAPI/Models/AngularjsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Client.WebAPI.Models
+{
+    public class AngularjsDbContext : DbContext
+    {
+        public AngularjsDbContext(DbContextOptions<AngularjsDbContext> options): base(options)
+        {
+                
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Backend/Client.WebAPI/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Client.WebAPI.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(35)]
+        public string Name { get; set; } = string.Empty;
+        
+        [StringLength(50, MinimumLength = 2)]
+        public string Address { get; set; } = string.Empty;
+        
+        [Required]
+        [MaxLength(50)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
